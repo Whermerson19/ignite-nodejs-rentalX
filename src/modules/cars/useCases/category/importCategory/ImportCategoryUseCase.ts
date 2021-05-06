fd0b8390ac37b818e424fb9e72deaeda9b758959
Whermerson19 <whermersonc@gmail.com>
2021-05-06 16:32:55 -0300

    lendo arquivo com conceito de stram do node

--- src/modules/cars/useCases/category/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/category/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,16 @@
+import fs from "fs";
+import csvParse from "csv-parse";
+
 export default class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
