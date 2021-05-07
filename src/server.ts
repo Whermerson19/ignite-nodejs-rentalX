079cdbaf23a9134820fc1810e661736750f05cb3
Whermerson19 <whermersonc@gmail.com>
2021-05-07 12:13:05 -0300

    criando a documentaçao de criaçao de categorias

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
+
 import appRouter from './routes';
 
 const app = express()
 app.use(express.json())
 
-app.listen(3333, () => console.log("server is running"))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(appRouter);
+
+app.listen(3333, () => console.log("server is running"))
